86fad15447a641e7b52ea6abfa7b8cb8cec02623
Abhisekprabhakar <kartik.abhishek98@gmail.com>
2021-08-03 19:54:23 +0530

    first commit

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
 
   <link href="{{ secure_asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
   <link href="{{ secure_asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-  <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
-  <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
-<link href="css/agency.min.css" rel="stylesheet">
+  <link href="{{ secure_asset('https://fonts.googleapis.com/css?family=Montserrat:400,700')}}" rel="stylesheet" type="text/css">
+  <link href="{{ secure_asset('https://fonts.googleapis.com/css?family=Kaushan+Script')}}" rel='stylesheet' type='text/css'>
+  <link href="{{ secure_asset('https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic')}}" rel='stylesheet' type='text/css'>
+  <link href="{{ secure_asset('https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet')}}"type='text/css'>
+<link href="{{ secure_asset('css/agency.min.css')}}" rel="stylesheet">
 <style type="text/css">
   img{height: 300px; width: 400px;}
 </style>
